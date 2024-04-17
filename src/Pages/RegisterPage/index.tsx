749966062baa6cc382ec5f784c41b057a8c015a4
Blindeen <kuba.szmajda@gmail.com>
2024-04-17 12:01:06 +0200

    add register page

---
+++ src/Pages/RegisterPage/index.tsx
@@ -0,0 +1,10 @@
+import ImageHero from 'Components/ImageHero';
+import Register from 'Containers/Register';
+
+const RegisterPage = () => (
+  <ImageHero imageurl='/assets/backgrounds/landing-page-background.jpg'>
+    <Register />
+  </ImageHero>
+);
+
+export default RegisterPage;
